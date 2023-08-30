3a7695bee257c7755896c5940d535ce3748a8775
Nguyễn Doãn Đạt <datndw@gmail.com>
2023-08-30 15:49:56 +0700

    datnd add exception middleware

---
+++ FDex.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FluentValidation.Results;
+
+namespace FDex.Application.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public List<string> Errors { get; set; } = new List<string>();
+        public ValidationException(ValidationResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                Errors.Add(error.ErrorMessage);
+            }
+        }
+    }
+}
+
