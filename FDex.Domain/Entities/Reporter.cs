b43868ed754d5dc1abcad7d764e7ad179e93c772
Nguyễn Doãn Đạt <datndw@gmail.com>
2023-10-02 05:09:10 +0700

    datnd add models

---
+++ FDex.Domain/Entities/Reporter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace FDex.Domain.Entities
+{
+	public class Reporter
+	{
+        public string Wallet { get; set; }
+        public int ReportCount { get; set; }
+        public DateTime LastReportedDate { get; set; }
+    }
+}
+
--- FDex.Domain/Entities/User.cs
+++ FDex.Domain/Entities/User.cs
@@ -4,6 +4,8 @@ namespace FDex.Domain.Entities
 	public class User
 	{
         public string Wallet { get; set; }
+        public string? ReferalToken { get; set; }
+        public uint? ReferalCode { get; set; }
         public DateTime CreatedDate { get; set; }
     }
 }
